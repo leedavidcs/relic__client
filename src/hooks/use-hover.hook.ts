bdd8a928831bdfb070fbf34973a99d01b6c099e0
Unknown <lee.david.cs@gmail.com>
2019-11-07 01:47:30 -0800

    Add hover hooks

---
+++ src/hooks/use-hover.hook.ts
@@ -0,0 +1,31 @@
+import { RefObject, useCallback, useEffect, useRef, useState } from "react";
+
+export const useHover = <T extends Element = Element>(
+	ref?: RefObject<T>
+): [boolean, RefObject<T>] => {
+	const [isHovered, setIsHovered] = useState<boolean>(false);
+
+	const createdRef: RefObject<T> = useRef<T>(null);
+	const hoverRef: RefObject<T> = ref || createdRef;
+
+	const onMouseOver = useCallback(() => setIsHovered(true), [setIsHovered]);
+	const onMouseOut = useCallback(() => setIsHovered(false), [setIsHovered]);
+
+	useEffect(() => {
+		const elem: T | null = hoverRef.current;
+
+		if (!elem) {
+			return;
+		}
+
+		elem.addEventListener("mouseover", onMouseOver);
+		elem.addEventListener("mouseout", onMouseOut);
+
+		return () => {
+			elem.removeEventListener("mouseover", onMouseOver);
+			elem.removeEventListener("mouseout", onMouseOut);
+		};
+	}, [hoverRef.current]);
+
+	return [isHovered, hoverRef];
+};
