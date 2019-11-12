0d5f687d90e3e6c63a99c2cf5df149392ce89f14
Unknown <lee.david.cs@gmail.com>
2019-11-12 02:17:23 -0800

    Fix transparent on theme. And color on overlay

--- src/components/input.component/button.component/index.tsx
+++ src/components/input.component/button.component/index.tsx
@@ -1,6 +1,7 @@
 import { Overlay } from "@/components/overlay.component";
 import { Ripple } from "@/components/ripple.component";
 import { useHover } from "@/hooks";
+import classnames from "classnames";
 import React, { ButtonHTMLAttributes, FC } from "react";
 import { useStyles } from "./styles";
 
@@ -18,19 +19,20 @@ const colors = [
 const sizes = ["small", "medium", "large"] as const;
 
 interface IProps {
+	className?: string;
 	color?: typeof colors[number];
 	onClick?: ButtonHTMLAttributes<HTMLButtonElement>["onClick"];
 	size?: typeof sizes[number];
 }
 
 export const Button: FC<IProps> = (props) => {
-	const { children, onClick } = props;
+	const { children, className, onClick } = props;
 
 	const classes = useStyles(props);
 	const [isHovered, hoverRef] = useHover<HTMLButtonElement>(false);
 
 	return (
-		<button ref={hoverRef} className={classes.root} onClick={onClick}>
+		<button ref={hoverRef} className={classnames(classes.root, className)} onClick={onClick}>
 			{children}
 			<Overlay active={isHovered} opacity={OVERLAY_OPACITY} />
 			<Ripple />
