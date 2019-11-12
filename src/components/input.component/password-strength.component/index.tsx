3ca0a00314d87ca3d2a599f3e9fabec87eb6b8c6
Unknown <lee.david.cs@gmail.com>
2019-11-12 10:37:01 -0800

    Add password strength component

---
+++ src/components/input.component/password-strength.component/index.tsx
@@ -0,0 +1,46 @@
+import classnames from "classnames";
+import React, { FC, useMemo } from "react";
+import zxcvbn from "zxcvbn";
+import { useStyles } from "./styles";
+
+const MAX_PERCENTAGE: number = 100;
+
+const strengths = ["Weak", "Weak", "Fair", "Good", "Strong"] as const;
+
+interface IProps {
+	className?: string;
+	password: string;
+}
+
+export const PasswordStrength: FC<IProps> = ({ className, password }) => {
+	const classes = useStyles();
+
+	const { score } = useMemo(() => zxcvbn(password), [password]);
+
+	const strength = strengths[score];
+
+	const percentage: number = useMemo(() => {
+		const portion: number = MAX_PERCENTAGE / (strengths.length - 1);
+
+		return portion * score;
+	}, [score]);
+
+	return (
+		<div className={classnames(classes.root, className)}>
+			<div className={classes.meter}>
+				<div
+					className={classnames(classes.meterFill, {
+						[classes.weak]: strength === "Weak",
+						[classes.fair]: strength === "Fair",
+						[classes.good]: strength === "Good",
+						[classes.strong]: strength === "Strong"
+					})}
+					style={{ width: `${percentage}%` }}
+				/>
+			</div>
+			<div className={classes.textWrapper}>
+				<strong>Password strength:</strong> {strength}
+			</div>
+		</div>
+	);
+};
---
+++ src/components/input.component/password-strength.component/stories.tsx
@@ -0,0 +1,19 @@
+import { Background } from "@/components/background.component";
+import { Paper } from "@/components/paper.component";
+import { text } from "@storybook/addon-knobs";
+import React from "react";
+import { PasswordStrength } from ".";
+
+export default { title: "input|password-strength", component: PasswordStrength };
+
+export const standard = () => {
+	const password = text("password", "");
+
+	return (
+		<Background>
+			<Paper>
+				<PasswordStrength password={password} />
+			</Paper>
+		</Background>
+	);
+};
