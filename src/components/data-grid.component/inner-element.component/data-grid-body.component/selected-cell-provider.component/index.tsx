4b79b24c89640ceb538a6c7f3d510236fcd2a3a2
Unknown <lee.david.cs@gmail.com>
2020-01-14 02:52:13 -0800

    Move selected cell provider to body. Unselect cell on click out

---
+++ src/components/data-grid.component/inner-element.component/data-grid-body.component/selected-cell-provider.component/index.tsx
@@ -0,0 +1,26 @@
+import { ClickOutside } from "@/components/click-outside.component";
+import React, { FC, ReactNode, useCallback, useMemo, useState } from "react";
+import { SelectedCellContext } from "./selected-cell.context";
+
+export * from "./selected-cell.context";
+
+interface IProps {
+	children: ReactNode;
+}
+
+export const SelectedCellProvider: FC<IProps> = ({ children }) => {
+	const [selectedCell, setSelectedCell] = useState<{ x: number; y: number } | null>(null);
+
+	const onClickOut = useCallback(() => setSelectedCell(null), [setSelectedCell]);
+
+	const value = useMemo(() => ({ selectedCell, setSelectedCell }), [
+		selectedCell,
+		setSelectedCell
+	]);
+
+	return (
+		<ClickOutside onClickOut={onClickOut}>
+			<SelectedCellContext.Provider value={value}>{children}</SelectedCellContext.Provider>
+		</ClickOutside>
+	);
+};
