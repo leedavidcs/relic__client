b0782023b827884737f2219537d336d3515ca8de
Unknown <lee.david.cs@gmail.com>
2020-01-18 05:10:00 -0800

    Move general util to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from "./array.util";
 export * from "./logger.util";
+export * from "./make-concurrent-func.util";
 export * from "./on-input-value-changed.util";
 export * from "./toggle-class.util";
