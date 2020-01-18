1e17d15f7d59b2028214909804ba8c07ef5bcb54
Unknown <lee.david.cs@gmail.com>
2020-01-18 05:22:55 -0800

    Revise click outside to work with portals

--- src/components/click-outside.component/index.ts
+++ src/components/click-outside.component/index.ts
@@ -4,22 +4,43 @@ import {
 	DetailedHTMLProps,
 	FC,
 	HTMLAttributes,
+	MouseEvent,
 	ReactElement,
 	useCallback,
+	useContext,
 	useEffect,
 	useRef
 } from "react";
+import { ClickOutsideContext } from "./click-outside-provider.component";
+
+export * from "./click-outside-provider.component";
 
 interface IProps {
+	/**
+	 * Children must be an HTMLElement, so that onClickCapture can be provided. Typescript does not
+	 * validate that the Children of ClickOutside has valid props when it is used.
+	 */
 	children: ReactElement<DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>>;
-	onClickOut: (event: MouseEvent | TouchEvent) => void;
+	/** Function to be invoked when the ReactNode is clicked outside of */
+	onClickOut: (event: MouseEvent<HTMLDivElement>) => void;
 }
 
 export const ClickOutside: FC<IProps> = ({ children, onClickOut }) => {
 	const didClickInside = useRef<boolean>(false);
 
+	const { register, unregister } = useContext(ClickOutsideContext);
+
+	const onClickCapture = useCallback(
+		(event: MouseEvent<HTMLElement>) => {
+			didClickInside.current = true;
+
+			Children.only(children).props.onClickCapture?.(event);
+		},
+		[children]
+	);
+
 	const listener = useCallback(
-		(event: MouseEvent | TouchEvent) => {
+		(event: MouseEvent<HTMLDivElement>) => {
 			if (didClickInside.current) {
 				didClickInside.current = false;
 
@@ -32,18 +53,10 @@ export const ClickOutside: FC<IProps> = ({ children, onClickOut }) => {
 	);
 
 	useEffect(() => {
-		document.addEventListener("mousedown", listener);
-		document.addEventListener("touchstart", listener);
+		const eventId: string = register(listener);
 
-		return () => {
-			document.removeEventListener("mousedown", listener);
-			document.removeEventListener("touchstart", listener);
-		};
-	}, [listener]);
-
-	const onClickCapture = useCallback(() => {
-		didClickInside.current = true;
-	}, []);
+		return () => unregister(eventId);
+	}, [listener, register, unregister]);
 
 	return cloneElement(Children.only(children), { onClickCapture });
 };
