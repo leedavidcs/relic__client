6c7b16cafda6c2c7cc3cac90347d9fc363453447
Unknown <lee.david.cs@gmail.com>
2020-01-22 02:24:12 -0800

    Fix overlay transition not working on active = false

--- src/components/overlay.component/index.tsx
+++ src/components/overlay.component/index.tsx
@@ -1,6 +1,6 @@
 import { toggleClass } from "@/utils";
 import classnames from "classnames";
-import React, { FC, RefObject, useEffect, useRef } from "react";
+import React, { FC, useEffect, useRef, useLayoutEffect } from "react";
 import { useStyles } from "./styles";
 
 interface IProps {
@@ -12,8 +12,18 @@ interface IProps {
 }
 
 export const Overlay: FC<IProps> = ({ active, animate, className, clickThrough, opacity }) => {
-	const classes = useStyles({ animate, clickThrough, opacity });
-	const elemRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+	const classes = useStyles({ active, animate, clickThrough, opacity });
+	const elemRef = useRef<HTMLDivElement>(null);
+
+	useLayoutEffect(() => {
+		const elem: HTMLDivElement | null = elemRef.current;
+
+		if (!elem) {
+			return;
+		}
+
+		toggleClass(elem, classes.active, true);
+	}, [active, classes]);
 
 	useEffect(() => {
 		const elem: HTMLDivElement | null = elemRef.current;
@@ -22,8 +32,8 @@ export const Overlay: FC<IProps> = ({ active, animate, className, clickThrough, 
 			return;
 		}
 
-		toggleClass(elem, classes.active, active);
-	}, [active, elemRef, classes.active]);
+		toggleClass(elem, classes.transition, true);
+	}, [animate, classes.transition]);
 
 	return <div className={classnames(classes.root, className)} ref={elemRef} />;
 };
