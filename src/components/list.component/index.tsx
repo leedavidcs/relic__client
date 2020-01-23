d196e951feeeca5e43b6f4163ebcee1f891fc19e
Unknown <lee.david.cs@gmail.com>
2020-01-23 06:57:10 -0800

    Add divider to context of list

--- src/components/list.component/index.tsx
+++ src/components/list.component/index.tsx
@@ -1,15 +1,25 @@
+import { DividerVariant } from "@/components/divider.component";
 import classnames from "classnames";
-import React, { FC } from "react";
+import React, { FC, useMemo } from "react";
+import { ListContext } from "./list.context";
 import { useStyles } from "./styles";
 
+export * from "./list.context";
 export * from "./list-item.component";
 
 interface IProps {
 	className?: string;
+	divider?: DividerVariant | null;
 }
 
-export const List: FC<IProps> = ({ children, className }) => {
+export const List: FC<IProps> = ({ children, divider = null, className }) => {
 	const classes = useStyles();
 
-	return <ul className={classnames(classes.root, className)}>{children}</ul>;
+	const value = useMemo(() => ({ divider }), [divider]);
+
+	return (
+		<ListContext.Provider value={value}>
+			<ul className={classnames(classes.root, className)}>{children}</ul>
+		</ListContext.Provider>
+	);
 };
