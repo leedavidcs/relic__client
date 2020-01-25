8b6a34bfcdb93c9afd04b6b1652978b30481b0f3
Unknown <lee.david.cs@gmail.com>
2020-01-24 21:50:39 -0800

    Move data-grid mock data generation out of the story component

--- src/components/data-grid.component/stories/standard.story.tsx
+++ src/components/data-grid.component/stories/standard.story.tsx
@@ -2,51 +2,42 @@ import { DataGrid, IHeaderConfig } from "@/components/data-grid.component";
 import { Paper } from "@/components/paper.component";
 import Faker from "faker";
 import { uniq } from "lodash";
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useCallback, useState } from "react";
 
 const MAX_DATA_SIZE = 100;
 const MAX_COLUMN_WIDTH = 80;
 const MIN_COLUMN_WIDTH = 30;
 const NUMBER_FROZEN_COLUMNS = 5;
 
+Faker.seed(1);
+
+const MOCK_HEADER_NAMES: readonly string[] = uniq(
+	[...Array(MAX_DATA_SIZE)].map(() => Faker.lorem.word())
+);
+
+const reducedSize: number = MOCK_HEADER_NAMES.length;
+
+const MOCK_DATA: readonly Record<string, number>[] = [...Array(reducedSize)].map((x, i) => {
+	const datum: Record<string, number> = {};
+
+	MOCK_HEADER_NAMES.forEach((name, j) => (datum[name] = j * reducedSize + i));
+
+	return datum;
+});
+
+const MOCK_HEADERS: readonly IHeaderConfig[] = MOCK_HEADER_NAMES.map((label, i) => ({
+	label,
+	value: label,
+	frozen: i < NUMBER_FROZEN_COLUMNS,
+	options: [{ label, value: label }],
+	resizable: true,
+	width: Faker.random.number({
+		max: MAX_COLUMN_WIDTH,
+		min: MIN_COLUMN_WIDTH
+	})
+}));
+
 export const StandardStory = () => {
-	Faker.seed(1);
-
-	const MOCK_HEADER_NAMES: string[] = useMemo(
-		() => uniq([...Array(MAX_DATA_SIZE)].map(() => Faker.lorem.word())),
-		[]
-	);
-
-	const reducedSize: number = MOCK_HEADER_NAMES.length;
-
-	const MOCK_DATA: { [key: string]: number }[] = useMemo(
-		() =>
-			[...Array(reducedSize)].map((x, i) => {
-				const datum: { [key: string]: any } = {};
-
-				MOCK_HEADER_NAMES.forEach((name, j) => (datum[name] = j * reducedSize + i));
-
-				return datum;
-			}),
-		[MOCK_HEADER_NAMES, reducedSize]
-	);
-
-	const MOCK_HEADERS: readonly IHeaderConfig[] = useMemo(
-		() =>
-			MOCK_HEADER_NAMES.map((label, i) => ({
-				label,
-				value: label,
-				frozen: i < NUMBER_FROZEN_COLUMNS,
-				options: [{ label, value: label }],
-				resizable: true,
-				width: Faker.random.number({
-					max: MAX_COLUMN_WIDTH,
-					min: MIN_COLUMN_WIDTH
-				})
-			})),
-		[MOCK_HEADER_NAMES]
-	);
-
 	const [data, setData] = useState<readonly { [key: string]: any }[]>(MOCK_DATA);
 	const [headers, setHeaders] = useState<readonly IHeaderConfig[]>(MOCK_HEADERS);
 
