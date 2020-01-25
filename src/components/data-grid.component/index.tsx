b6b43737856dfde464733d6a308ca24a5dd2416c
Unknown <lee.david.cs@gmail.com>
2020-01-25 00:00:04 -0800

    Memoize data-grid component, and add defaults to prop comments

--- src/components/data-grid.component/index.tsx
+++ src/components/data-grid.component/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, memo } from "react";
 import { FixedSizeList } from "react-window";
 import { DataGridProvider } from "./data-grid-provider.component";
 import { DataRow } from "./data-row.component";
@@ -30,47 +30,63 @@ export interface IHeaderConfig extends IHeaderOption {
 
 interface IProps {
 	/** Entities array */
-	data: readonly { [key: string]: any }[];
+	data: readonly Record<string, any>[];
 	/** Column data is: `data[headers[i].value]` */
 	headers: readonly IHeaderConfig[];
-	/** Function to derive the key prop for each row of the data-grid. Defaults to index. */
-	itemKey?: (index: number, data: readonly { [key: string]: any }[]) => string;
-	/** `data` is a controlled property, to be set externally through `onDataChange` */
-	onDataChange: (data: readonly { [key: string]: any }[]) => void;
-	/** `headers` is a controlled property, to be set externally through `onHeadersChange` */
-	onHeadersChange: (headers: readonly IHeaderConfig[]) => void;
+	/**
+	 * Function to derive the key prop for each row of the data-grid. Defaults to index.
+	 *
+	 * @default (index: number) => index.toString()
+	 */
+	itemKey?: (index: number, data: readonly Record<string, any>[]) => string;
+	/**
+	 * `data` is a controlled property, to be set externally through `onDataChange`
+	 *
+	 * @default () => undefined
+	 */
+	onDataChange?: (data: readonly Record<string, any>[]) => void;
+	/**
+	 * `headers` is a controlled property, to be set externally through `onHeadersChange`
+	 *
+	 * @default () => undefined
+	 */
+	onHeadersChange?: (headers: readonly IHeaderConfig[]) => void;
 }
 
-export const DataGrid: FC<IProps> = ({
-	data,
-	headers,
-	itemKey = (index: number) => index,
-	onHeadersChange,
-	onDataChange
-}) => {
-	const itemCount: number = data.length;
+export const DataGrid: FC<IProps> = memo(
+	({
+		data,
+		headers,
+		itemKey = (index: number) => index.toString(),
+		onDataChange = () => undefined,
+		onHeadersChange = () => undefined
+	}) => {
+		const itemCount: number = data.length;
 
-	return (
-		<DataGridProvider
-			data={data}
-			onDataChange={onDataChange}
-			headers={headers}
-			onHeadersChange={onHeadersChange}
-		>
-			{({ height, width }) => (
-				<FixedSizeList
-					height={height}
-					width={width}
-					itemCount={itemCount}
-					itemData={data}
-					itemKey={itemKey}
-					itemSize={DEFAULT_ROW_HEIGHT}
-					innerElementType={InnerElement}
-					outerElementType={OuterElement}
-				>
-					{DataRow}
-				</FixedSizeList>
-			)}
-		</DataGridProvider>
-	);
-};
+		return (
+			<DataGridProvider
+				data={data}
+				onDataChange={onDataChange}
+				headers={headers}
+				onHeadersChange={onHeadersChange}
+			>
+				{({ height, width }) => (
+					<FixedSizeList
+						height={height}
+						width={width}
+						itemCount={itemCount}
+						itemData={data}
+						itemKey={itemKey}
+						itemSize={DEFAULT_ROW_HEIGHT}
+						innerElementType={InnerElement}
+						outerElementType={OuterElement}
+					>
+						{DataRow}
+					</FixedSizeList>
+				)}
+			</DataGridProvider>
+		);
+	}
+);
+
+DataGrid.displayName = "DataGrid";
