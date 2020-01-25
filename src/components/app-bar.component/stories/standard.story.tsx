57187c90631f3df8908041df7825c592265317ba
Unknown <lee.david.cs@gmail.com>
2020-01-24 21:37:58 -0800

    Add app bar mdx

--- src/components/app-bar.component/stories/standard.story.tsx
+++
@@ -1,9 +0,0 @@
-import { AppBar } from "@/components/app-bar.component";
-import { action } from "@storybook/addon-actions";
-import React from "react";
-
-export const StandardStory = () => {
-	const onClickSignOut = action("sign out");
-
-	return <AppBar onClickSignOut={onClickSignOut} title="Title goes here" user={null} />;
-};
