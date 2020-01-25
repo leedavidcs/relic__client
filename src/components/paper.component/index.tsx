1fa722835c5c0aad8690a59d2037e364a790b7be
Unknown <lee.david.cs@gmail.com>
2020-01-25 05:09:57 -0800

    Simplify paper props declaration

--- src/components/paper.component/index.tsx
+++ src/components/paper.component/index.tsx
@@ -1,24 +1,20 @@
 import classnames from "classnames";
-import React, { DetailedHTMLProps, forwardRef, HTMLAttributes, ReactNode } from "react";
+import React, { DetailedHTMLProps, forwardRef, HTMLAttributes } from "react";
 import { useStyles } from "./styles";
 
-interface IProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-	children?: ReactNode;
-	className?: string;
-}
+export const Paper = forwardRef<
+	HTMLDivElement,
+	DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+>(({ className, children, ...divProps }, ref) => {
+	const classes = useStyles();
 
-export const Paper = forwardRef<HTMLDivElement, IProps>(
-	({ className, children, ...divProps }, ref) => {
-		const classes = useStyles();
+	const classNames: string = classnames(classes.root, className);
 
-		const classNames: string = classnames(classes.root, className);
-
-		return (
-			<div className={classNames} {...divProps} ref={ref}>
-				{children}
-			</div>
-		);
-	}
-);
+	return (
+		<div className={classNames} {...divProps} ref={ref}>
+			{children}
+		</div>
+	);
+});
 
 Paper.displayName = "Paper";
