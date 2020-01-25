2a084eb628c858feb5aa580fac8e01e2f58df60b
Unknown <lee.david.cs@gmail.com>
2020-01-24 21:00:41 -0800

    Fix using removed dependency on welcome story

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -1,7 +1,6 @@
-import { linkTo } from "@storybook/addon-links";
 import { Welcome } from "@storybook/react/demo";
 import React from "react";
 
 export default { title: "Welcome", component: Welcome };
 
-export const toStorybook = () => <Welcome showApp={linkTo("Button")} />;
+export const toStorybook = () => <Welcome />;
