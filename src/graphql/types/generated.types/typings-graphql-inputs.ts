92bd9c1dc0d32713c1aa3e2e886260ad032e1212
leedavidcs <lee.david.cs@gmail.com>
2020-02-20 05:32:27 -0800

    Remove server-side graphql type generation. Regenerated types
    Remove server-side graphql type generation. Regenerated types
    
    
    Remove server-side graphql type generation. Regenerated types
    
    
    Remove server-side graphql type generation. Regenerated types

--- src/graphql/types/generated.types/typings-graphql-inputs.ts
+++ src/graphql/types/generated.types/typings-graphql-inputs.ts
@@ -8,26 +8,26 @@
 // ====================================================
 
 export interface LoginLocalUser_loginLocalUser {
-	readonly __typename: "TokenPayload";
-	/**
-	 * JSON web token to authenticate API requests
-	 */
-	readonly token: string;
-	/**
-	 * JSON web token to refresh the token
-	 */
-	readonly refreshToken: string;
+  readonly __typename: "TokenPayload";
+  /**
+   * JSON web token to authenticate API requests
+   */
+  readonly token: string;
+  /**
+   * JSON web token to refresh the token
+   */
+  readonly refreshToken: string;
 }
 
 export interface LoginLocalUser {
-	/**
-	 * Logins in the user, and returns an expiring access token
-	 */
-	readonly loginLocalUser: LoginLocalUser_loginLocalUser | null;
+  /**
+   * Logins in the user, and returns an expiring access token
+   */
+  readonly loginLocalUser: LoginLocalUser_loginLocalUser | null;
 }
 
 export interface LoginLocalUserVariables {
-	readonly input: LoginLocalUserInput;
+  readonly input: LoginLocalUserInput;
 }
 
 /* tslint:disable */
@@ -40,26 +40,26 @@ export interface LoginLocalUserVariables {
 // ====================================================
 
 export interface RefreshAccessToken_refreshAccessToken {
-	readonly __typename: "TokenPayload";
-	/**
-	 * JSON web token to authenticate API requests
-	 */
-	readonly token: string;
-	/**
-	 * JSON web token to refresh the token
-	 */
-	readonly refreshToken: string;
+  readonly __typename: "TokenPayload";
+  /**
+   * JSON web token to authenticate API requests
+   */
+  readonly token: string;
+  /**
+   * JSON web token to refresh the token
+   */
+  readonly refreshToken: string;
 }
 
 export interface RefreshAccessToken {
-	/**
-	 * Refreshes the currently logged-in user's access token
-	 */
-	readonly refreshAccessToken: RefreshAccessToken_refreshAccessToken | null;
+  /**
+   * Refreshes the currently logged-in user's access token
+   */
+  readonly refreshAccessToken: RefreshAccessToken_refreshAccessToken | null;
 }
 
 export interface RefreshAccessTokenVariables {
-	readonly input: RefreshAccessTokenInput;
+  readonly input: RefreshAccessTokenInput;
 }
 
 /* tslint:disable */
@@ -72,50 +72,50 @@ export interface RefreshAccessTokenVariables {
 // ====================================================
 
 export interface RegisterLocalUser_registerLocalUser_user {
-	readonly __typename: "User";
-	/**
-	 * The id of the user
-	 */
-	readonly id: string;
-	/**
-	 * The user's email
-	 */
-	readonly email: any;
-	/**
-	 * Whether the user verified their email address
-	 */
-	readonly emailVerified: boolean;
-	/**
-	 * The user's username
-	 */
-	readonly username: string;
+  readonly __typename: "User";
+  /**
+   * The id of the user
+   */
+  readonly id: string;
+  /**
+   * The user's email
+   */
+  readonly email: any;
+  /**
+   * Whether the user verified their email address
+   */
+  readonly emailVerified: boolean;
+  /**
+   * The user's username
+   */
+  readonly username: string;
 }
 
 export interface RegisterLocalUser_registerLocalUser {
-	readonly __typename: "RegisterLocalUserPayload";
-	/**
-	 * Whether the registration successfully created a user or not
-	 */
-	readonly success: boolean;
-	/**
-	 * An error will be described if success is false
-	 */
-	readonly error: string | null;
-	/**
-	 * The user object
-	 */
-	readonly user: RegisterLocalUser_registerLocalUser_user | null;
+  readonly __typename: "RegisterLocalUserPayload";
+  /**
+   * Whether the registration successfully created a user or not
+   */
+  readonly success: boolean;
+  /**
+   * An error will be described if success is false
+   */
+  readonly error: string | null;
+  /**
+   * The user object
+   */
+  readonly user: RegisterLocalUser_registerLocalUser_user | null;
 }
 
 export interface RegisterLocalUser {
-	/**
-	 * Performs local authentication (custom username + password)
-	 */
-	readonly registerLocalUser: RegisterLocalUser_registerLocalUser | null;
+  /**
+   * Performs local authentication (custom username + password)
+   */
+  readonly registerLocalUser: RegisterLocalUser_registerLocalUser | null;
 }
 
 export interface RegisterLocalUserVariables {
-	readonly input: RegisterLocalUserInput;
+  readonly input: RegisterLocalUserInput;
 }
 
 /* tslint:disable */
@@ -128,10 +128,10 @@ export interface RegisterLocalUserVariables {
 // ====================================================
 
 export interface ResendVerifyEmail {
-	/**
-	 * Resends the account verification email to the logged-in user
-	 */
-	readonly resendVerifyEmail: boolean;
+  /**
+   * Resends the account verification email to the logged-in user
+   */
+  readonly resendVerifyEmail: boolean;
 }
 
 /* tslint:disable */
@@ -144,31 +144,31 @@ export interface ResendVerifyEmail {
 // ====================================================
 
 export interface SetUser_setUser {
-	readonly __typename: "User";
-	/**
-	 * The id of the user
-	 */
-	readonly id: string;
-	/**
-	 * The user's email
-	 */
-	readonly email: any;
-	/**
-	 * Whether the user verified their email address
-	 */
-	readonly emailVerified: boolean;
-	/**
-	 * The user's username
-	 */
-	readonly username: string;
+  readonly __typename: "User";
+  /**
+   * The id of the user
+   */
+  readonly id: string;
+  /**
+   * The user's email
+   */
+  readonly email: any;
+  /**
+   * Whether the user verified their email address
+   */
+  readonly emailVerified: boolean;
+  /**
+   * The user's username
+   */
+  readonly username: string;
 }
 
 export interface SetUser {
-	readonly setUser: SetUser_setUser | null;
+  readonly setUser: SetUser_setUser | null;
 }
 
 export interface SetUserVariables {
-	readonly user?: UserInput | null;
+  readonly user?: UserInput | null;
 }
 
 /* tslint:disable */
@@ -181,11 +181,11 @@ export interface SetUserVariables {
 // ====================================================
 
 export interface ToggleModal {
-	readonly toggleModal: boolean;
+  readonly toggleModal: boolean;
 }
 
 export interface ToggleModalVariables {
-	readonly force?: boolean | null;
+  readonly force?: boolean | null;
 }
 
 /* tslint:disable */
@@ -198,7 +198,7 @@ export interface ToggleModalVariables {
 // ====================================================
 
 export interface GetModal {
-	readonly modal: boolean;
+  readonly modal: boolean;
 }
 
 /* tslint:disable */
@@ -211,27 +211,27 @@ export interface GetModal {
 // ====================================================
 
 export interface GetUser_user {
-	readonly __typename: "User";
-	/**
-	 * The id of the user
-	 */
-	readonly id: string;
-	/**
-	 * The user's email
-	 */
-	readonly email: any;
-	/**
-	 * Whether the user verified their email address
-	 */
-	readonly emailVerified: boolean;
-	/**
-	 * The user's username
-	 */
-	readonly username: string;
+  readonly __typename: "User";
+  /**
+   * The id of the user
+   */
+  readonly id: string;
+  /**
+   * The user's email
+   */
+  readonly email: any;
+  /**
+   * Whether the user verified their email address
+   */
+  readonly emailVerified: boolean;
+  /**
+   * The user's username
+   */
+  readonly username: string;
 }
 
 export interface GetUser {
-	readonly user: GetUser_user | null;
+  readonly user: GetUser_user | null;
 }
 
 /* tslint:disable */
@@ -244,30 +244,30 @@ export interface GetUser {
 // ====================================================
 
 export interface GetViewer_viewer {
-	readonly __typename: "Viewer";
-	/**
-	 * The viewer's id
-	 */
-	readonly id: string;
-	/**
-	 * The viewer's email
-	 */
-	readonly email: string;
-	/**
-	 * Whether this viewer verified their email address
-	 */
-	readonly emailVerified: boolean;
-	/**
-	 * The viewer's username
-	 */
-	readonly username: string;
+  readonly __typename: "Viewer";
+  /**
+   * The viewer's id
+   */
+  readonly id: string;
+  /**
+   * The viewer's email
+   */
+  readonly email: string;
+  /**
+   * Whether this viewer verified their email address
+   */
+  readonly emailVerified: boolean;
+  /**
+   * The viewer's username
+   */
+  readonly username: string;
 }
 
 export interface GetViewer {
-	/**
-	 * The viewer of this request
-	 */
-	readonly viewer: GetViewer_viewer | null;
+  /**
+   * The viewer of this request
+   */
+  readonly viewer: GetViewer_viewer | null;
 }
 
 /* tslint:disable */
@@ -280,25 +280,25 @@ export interface GetViewer {
 //==============================================================
 
 export interface LoginLocalUserInput {
-	readonly userIdentifier: string;
-	readonly password: string;
+  readonly userIdentifier: string;
+  readonly password: string;
 }
 
 export interface RefreshAccessTokenInput {
-	readonly refreshToken: string;
+  readonly refreshToken: string;
 }
 
 export interface RegisterLocalUserInput {
-	readonly email: any;
-	readonly password: any;
-	readonly username: string;
+  readonly email: any;
+  readonly password: any;
+  readonly username: string;
 }
 
 export interface UserInput {
-	readonly id: string;
-	readonly email: any;
-	readonly emailVerified: boolean;
-	readonly username: string;
+  readonly id: string;
+  readonly email: any;
+  readonly emailVerified: boolean;
+  readonly username: string;
 }
 
 //==============================================================
--- src/typings-graphql.d.ts
+++ src/typings-graphql.d.ts
@@ -1,71 +1,81 @@
-declare module "*/login-local-user.mutation.graphql" {
-	import { DocumentNode } from "graphql";
-	const defaultDocument: DocumentNode;
-	export const LoginLocalUser: DocumentNode;
 
-	export default defaultDocument;
+declare module '*/login-local-user.mutation.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const LoginLocalUser: DocumentNode;
+
+  export default defaultDocument;
 }
+    
 
-declare module "*/refresh-access-token.mutation.graphql" {
-	import { DocumentNode } from "graphql";
-	const defaultDocument: DocumentNode;
-	export const RefreshAccessToken: DocumentNode;
+declare module '*/refresh-access-token.mutation.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const RefreshAccessToken: DocumentNode;
 
-	export default defaultDocument;
+  export default defaultDocument;
 }
+    
 
-declare module "*/register-local-user.mutation.graphql" {
-	import { DocumentNode } from "graphql";
-	const defaultDocument: DocumentNode;
-	export const RegisterLocalUser: DocumentNode;
+declare module '*/register-local-user.mutation.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const RegisterLocalUser: DocumentNode;
 
-	export default defaultDocument;
+  export default defaultDocument;
 }
+    
 
-declare module "*/resend-verify-email.mutation.graphql" {
-	import { DocumentNode } from "graphql";
-	const defaultDocument: DocumentNode;
-	export const ResendVerifyEmail: DocumentNode;
+declare module '*/resend-verify-email.mutation.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const ResendVerifyEmail: DocumentNode;
 
-	export default defaultDocument;
+  export default defaultDocument;
 }
+    
 
-declare module "*/set-user.mutation.graphql" {
-	import { DocumentNode } from "graphql";
-	const defaultDocument: DocumentNode;
-	export const SetUser: DocumentNode;
+declare module '*/set-user.mutation.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const SetUser: DocumentNode;
 
-	export default defaultDocument;
+  export default defaultDocument;
 }
+    
 
-declare module "*/toggle-modal.mutation.graphql" {
-	import { DocumentNode } from "graphql";
-	const defaultDocument: DocumentNode;
-	export const ToggleModal: DocumentNode;
+declare module '*/toggle-modal.mutation.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const ToggleModal: DocumentNode;
 
-	export default defaultDocument;
+  export default defaultDocument;
 }
+    
 
-declare module "*/get-modal.query.graphql" {
-	import { DocumentNode } from "graphql";
-	const defaultDocument: DocumentNode;
-	export const GetModal: DocumentNode;
+declare module '*/get-modal.query.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const GetModal: DocumentNode;
 
-	export default defaultDocument;
+  export default defaultDocument;
 }
+    
 
-declare module "*/get-user.query.graphql" {
-	import { DocumentNode } from "graphql";
-	const defaultDocument: DocumentNode;
-	export const GetUser: DocumentNode;
+declare module '*/get-user.query.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const GetUser: DocumentNode;
 
-	export default defaultDocument;
+  export default defaultDocument;
 }
+    
 
-declare module "*/get-viewer.query.graphql" {
-	import { DocumentNode } from "graphql";
-	const defaultDocument: DocumentNode;
-	export const GetViewer: DocumentNode;
+declare module '*/get-viewer.query.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const GetViewer: DocumentNode;
 
-	export default defaultDocument;
+  export default defaultDocument;
 }
+    
