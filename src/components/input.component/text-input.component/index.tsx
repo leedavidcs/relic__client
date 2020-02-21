775e363b5af953fe97a04de8c32ba2a02540127a
leedavidcs <lee.david.cs@gmail.com>
2020-02-21 06:48:03 -0800

    Rewrite text input to work with react-hook-form
    Rewrite text input to work with react-hook-form

--- src/components/input.component/text-input.component/index.tsx
+++ src/components/input.component/text-input.component/index.tsx
@@ -1,12 +1,11 @@
-import { useFocus } from "@/hooks";
+import { useFocus, useUnifiedRef } from "@/hooks";
 import { toggleClass } from "@/utils";
 import classnames from "classnames";
 import React, {
 	CSSProperties,
-	FC,
 	FormEvent,
+	forwardRef,
 	KeyboardEvent,
-	MouseEvent,
 	ReactElement,
 	RefObject,
 	useCallback,
@@ -24,14 +23,14 @@ interface IProps {
 	className?: string;
 	/** Whether the text-input should be disabled */
 	disabled?: boolean;
+	/** Error  */
+	error?: null | boolean | string;
 	/** Label to be placed on the text input. Acts as a placeholder as well */
 	label: string;
+	/** Name given to the text input (for submitting a form) */
+	name?: string;
 	/** HTMLInputElement onChange event */
 	onChange?: (event: FormEvent<HTMLInputElement>) => void;
-	/** HTMLInputElement onClick event */
-	onClick?: (event: MouseEvent<HTMLInputElement>) => void;
-	/** HTMLInputElement onInput event */
-	onInput?: (event: FormEvent<HTMLInputElement>) => void;
 	/** HTMLInputElement onKeyDown event */
 	onKeyDown?: (event: KeyboardEvent<HTMLInputElement>) => void;
 	/** Whether this should censor inputs (as a password input) */
@@ -42,143 +41,139 @@ interface IProps {
 	startIcon?: ReactElement;
 	/** Optional styles to pass to the outermost `div` */
 	style?: CSSProperties;
-	/**
-	 * Validator.
-	 *
-	 * If a string is passed, and there is no input, and the input is dirty, this will emit the
-	 * string as an error message.
-	 *
-	 * If null is passed, there is no validation
-	 *
-	 * If a function is passed, it will emit an error message if a string is returned.
-	 */
-	validator?: string | null | ((value: string) => string | null);
 	/** Controlled value of the input */
-	value: string;
+	value?: string;
 	/** Stylistic variations for the element. See story */
 	variant?: TextInputVariant;
 }
 
-export const TextInput: FC<IProps> = ({
-	className,
-	disabled = false,
-	label,
-	onChange,
-	onClick,
-	onInput: propsOnInput,
-	onKeyDown,
-	password = false,
-	spellCheck = false,
-	startIcon: propsStartIcon,
-	style,
-	value,
-	validator = () => null,
-	variant = "underlined"
-}) => {
-	const classes = useStyles({ variant });
+const getErrorMessage = (
+	label: string,
+	error: null | boolean | string,
+	fallback = label
+): string => {
+	switch (typeof error) {
+		case "boolean":
+			// Error message is not provided. Use fallback if error
+			return error ? fallback : label;
+		case "string":
+			// Error message is provided (is string). Use error
+			return error;
+		default:
+			// Error is null: There is no error, use label
+			return label;
+	}
+};
 
-	const [dirty, setDirty] = useState<boolean>(false);
-	const [hasInput, setHasInput] = useState<boolean>(false);
-	const [isValidInput, setIsValidInput] = useState<boolean>(true);
-	const [labelText, setLabelText] = useState<string>(label);
+export const TextInput = forwardRef<HTMLInputElement, IProps>(
+	(
+		{
+			className,
+			disabled = false,
+			error = null,
+			label,
+			name,
+			onChange: propsOnChange,
+			onKeyDown,
+			password = false,
+			spellCheck = false,
+			startIcon: propsStartIcon = null,
+			style,
+			value,
+			variant = "underlined"
+		},
+		ref
+	) => {
+		const classes = useStyles({ variant });
 
-	const rootRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
-	const labelRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
-	const inputRef: RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null);
+		const [hasInput, setHasInput] = useState<boolean>(false);
+		const [labelText, setLabelText] = useState<string>(label);
 
-	const [isFocused] = useFocus(false, inputRef);
+		const rootRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+		const labelRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+		const inputRef: RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null);
+		const unifiedRef = useUnifiedRef<HTMLInputElement>(ref, inputRef);
 
-	const onInput = useCallback(
-		(event: FormEvent<HTMLInputElement>): void => {
-			if (propsOnInput) {
-				propsOnInput(event);
+		const [isFocused] = useFocus(false, inputRef);
+
+		const hasError: boolean =
+			typeof error === "string" || (typeof error === "boolean" && error);
+
+		const onChange = useCallback(
+			(event: FormEvent<HTMLInputElement>) => {
+				propsOnChange?.(event);
+				setHasInput(Boolean(event.currentTarget.value));
+			},
+			[propsOnChange]
+		);
+
+		useEffect(() => setLabelText(getErrorMessage(label, error)), [error, setLabelText, label]);
+
+		useEffect(() => {
+			const labelDiv: HTMLDivElement | null = labelRef.current;
+
+			if (labelDiv === null) {
+				return;
 			}
 
-			setDirty(true);
-		},
-		[propsOnInput, setDirty]
-	);
+			labelDiv.className = classnames(classes.label, {
+				[classes.labelActive]: hasInput,
+				[classes.invalid]: Boolean(error)
+			});
+		}, [error, hasInput, classes.labelActive, classes.invalid, classes.label]);
 
-	useEffect(() => {
-		if (!dirty) {
-			return;
-		}
+		useEffect(() => {
+			const rootDiv: HTMLDivElement | null = rootRef.current;
 
-		const validatorMessage: string | null =
-			typeof validator === "function" ? validator(value) : validator;
-		const isValid: boolean = validatorMessage === null;
+			if (rootDiv === null) {
+				return;
+			}
 
-		setLabelText(validatorMessage === null ? label : validatorMessage);
+			toggleClass(rootDiv, classes.focused, isFocused);
+		}, [classes.focused, isFocused]);
 
-		setIsValidInput(isValid);
-		setHasInput(value.length > 0);
-	}, [dirty, validator, setLabelText, label, value]);
+		const startIcon = useMemo(
+			() =>
+				propsStartIcon && <div className={classes.startIconWrapper}>{propsStartIcon}</div>,
+			[classes, propsStartIcon]
+		);
 
-	useEffect(() => {
-		const labelDiv: HTMLDivElement | null = labelRef.current;
-
-		if (labelDiv === null) {
-			return;
-		}
-
-		labelDiv.className = classnames(classes.label, {
-			[classes.labelActive]: hasInput,
-			[classes.invalid]: !isValidInput
-		});
-	}, [hasInput, isValidInput, classes.labelActive, classes.invalid, classes.label]);
-
-	useEffect(() => {
-		const rootDiv: HTMLDivElement | null = rootRef.current;
-
-		if (rootDiv === null) {
-			return;
-		}
-
-		toggleClass(rootDiv, classes.focused, isFocused);
-	}, [classes.focused, isFocused]);
-
-	const startIcon = useMemo(
-		() =>
-			propsStartIcon ? (
-				<div className={classes.startIconWrapper}>{propsStartIcon}</div>
-			) : null,
-		[classes, propsStartIcon]
-	);
-
-	return (
-		<div
-			ref={rootRef}
-			className={classnames(classes.root, className, {
-				[classes.invalid]: !isValidInput
-			})}
-			style={style}
-		>
-			{startIcon}
-			<div className={classes.textInputWrapper}>
-				<div
-					ref={labelRef}
-					className={classnames(classes.label, {
-						[classes.invalid]: !isValidInput
-					})}
-				>
-					<div>{labelText}</div>
+		return (
+			<div
+				ref={rootRef}
+				className={classnames(classes.root, className, {
+					[classes.invalid]: hasError
+				})}
+				style={style}
+			>
+				{startIcon}
+				<div className={classes.textInputWrapper}>
+					<div
+						ref={labelRef}
+						className={classnames(classes.label, {
+							[classes.invalid]: hasError
+						})}
+					>
+						<div>{labelText}</div>
+					</div>
+					<input
+						ref={unifiedRef}
+						className={classnames(classes.textInput, {
+							[classes.invalid]: hasError
+						})}
+						type={password ? "password" : "text"}
+						disabled={disabled}
+						name={name}
+						onChange={onChange}
+						onKeyDown={onKeyDown}
+						spellCheck={spellCheck}
+						value={value}
+					/>
+					<div />
 				</div>
-				<input
-					ref={inputRef}
-					className={classnames(classes.textInput, {
-						[classes.invalid]: !isValidInput
-					})}
-					type={password ? "password" : "text"}
-					disabled={disabled}
-					onChange={onChange}
-					onClick={onClick}
-					onInput={onInput}
-					onKeyDown={onKeyDown}
-					spellCheck={spellCheck}
-					value={value}
-				/>
-				<div />
 			</div>
-		</div>
-	);
-};
+		);
+	}
+);
+
+TextInput.displayName = "TextInput";
